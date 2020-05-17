1abefcd2e102130e85b6aa223600de68c2efdbe8
abemyatt <abe.myatt@hotmail.com>
2020-05-17 13:40:05 +0100

    Use inheritance for unit tests

--- src/test/java/abemyatt/games/service/GameServiceTest.java
+++ src/test/java/abemyatt/games/service/GameServiceTest.java
@@ -6,11 +6,10 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
+import abemyatt.games.GameTestParent;
 import abemyatt.games.model.AverageLike;
-import abemyatt.games.model.Comment;
 import abemyatt.games.model.Game;
 import abemyatt.games.repository.GameRepository;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -22,54 +21,13 @@ import java.util.List;
 import java.util.Optional;
 
 @ExtendWith(MockitoExtension.class)
-class GameServiceTest {
-    private static final long TIME_IN_EPOCH = 1589760000L;
-    private static final long TIME_IN_EPOCH_TWO = 1583107200L;
-    private static final String GAME_TITLE = "The Game";
-    private static final String GAME_TITLE_TWO = "Cool Game";
-    private static final String USER = "John Smith";
-    private static final String USER_TWO = "Bob";
-    private static final String USER_THREE = "Alice";
+class GameServiceTest extends GameTestParent {
 
     @Mock
     private GameRepository repository;
 
     @InjectMocks
     private GameServiceImpl service;
-
-    private List<Game> games;
-    private Game game;
-
-    private List<AverageLike> averageLikes;
-
-    @BeforeEach
-    void setup() {
-        // Setup the comments for the games
-        ArrayList<Comment> comments = new ArrayList<>();
-        comments.add(new Comment(USER, "This is a comment", TIME_IN_EPOCH, 10));
-        comments.add(new Comment(USER_TWO, "I am writing a comment", TIME_IN_EPOCH_TWO, 3));
-
-        List<Comment> commentsTwo = new ArrayList<>();
-        commentsTwo.add(new Comment(USER, "This is another comment", TIME_IN_EPOCH, 20));
-        commentsTwo.add(new Comment(USER_THREE, "I am writing a comment like Bob", TIME_IN_EPOCH_TWO, 20));
-
-        // Setup the game object and games arrays
-        ArrayList<String> platforms = new ArrayList<>();
-        platforms.add("PC");
-        platforms.add("XBOX");
-        game = new Game("1", GAME_TITLE, "You just lost the game",
-                "Abe Myatt", platforms, "18", 100, comments);
-        Game gameTwo = new Game("2", GAME_TITLE_TWO, "This is a cool game",
-                "Graham", platforms, "3", 20, commentsTwo);
-        games = new ArrayList<>();
-        games.add(game);
-        games.add(gameTwo);
-
-        // Setup expected average likes by adding the comment like values and averaging
-        averageLikes = new ArrayList<>();
-        averageLikes.add(new AverageLike(GAME_TITLE, (10 + 3) / 2));
-        averageLikes.add(new AverageLike(GAME_TITLE_TWO, (20 + 20) / 2));
-    }
 
     @Test
     void testFindByIdExists() {
