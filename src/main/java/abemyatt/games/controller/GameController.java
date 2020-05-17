3aa1f1cb14e24fcabe68b8a7e3cc5f339c157dba
abemyatt <abe.myatt@hotmail.com>
2020-05-18 00:25:32 +0100

    refactor controller returns to use response entity

--- src/main/java/abemyatt/games/controller/GameController.java
+++ src/main/java/abemyatt/games/controller/GameController.java
@@ -4,7 +4,12 @@ import abemyatt.games.model.Game;
 import abemyatt.games.model.Summary;
 import abemyatt.games.service.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/games")
@@ -18,13 +23,25 @@ public class GameController {
     }
 
     @GetMapping("/{id}")
-    public Game findGameById(@PathVariable String id) {
-        return service.findById(id);
+    public ResponseEntity findGameById(@PathVariable String id) {
+        Game game = service.findById(id);
+
+        if (game == null) {
+            return new ResponseEntity<HttpStatus>(HttpStatus.NOT_FOUND);
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(game);
     }
 
     @GetMapping("/report")
-    public Summary getGameSummary() {
-        return service.getGameSummary();
+    public ResponseEntity getGameSummary() {
+        Summary summary = service.getGameSummary();
+
+        if (summary == null) {
+            return new ResponseEntity<HttpStatus>(HttpStatus.NOT_FOUND);
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(summary);
     }
 
 }
